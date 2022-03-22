f9a7c6de9063774cb8b86516f2fa90d0bb2713b1
Jake Merlino <101665796+JakeV-Graysonschool@users.noreply.github.com>
2022-03-22 19:09:34 +0000

    Wrote iteration, printing, retrieving, and setting methods

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 class LinkedList:
   
   class __Node:
-
+    
     def __init__(self,item,next=None):
       self.item = item
       self.next = next
@@ -30,9 +30,42 @@ class LinkedList:
     self.last.set_next(node)
     self.last = node
     self.num_items += 1
-    
+
+  def __iter__(self):
+    cursor = self.first.get_next()
+    while cursor != None:
+      yield cursor.get_item()
+      cursor = cursor.get_next()
+
+  def __str__(self):
+    strist = "["
+    for e in self:
+      strist += str(e) + ","
+    strist = strist.rstrip(",")
+    strist += "]"
+    return strist
+
+  def __getitem__(self,index):
+    if 0 <= index < self.num_items:
+      cursor = self.first.get_next()
+      for i in range(index):
+        cursor = cursor.get_next()
+      return cursor.get_item()
+    raise IndexError("LinkedList index is out of range.")
+  def __setitem__(self,index,val):
+    if 0 <= index < self.num_items:
+      cursor = self.first.get_next()
+      for i in range(index):
+        cursor = cursor.get_next()
+      cursor.set_item(val)
+      return
+    raise IndexError("LinkedList index is out of range.")
 def main():
-  test_list = LinkedList([1,2,3])
+  test_list = LinkedList(range(5))
+  for e in test_list:
+    print(e)
+  print(test_list[0])
+  test_list[2] = 22
   print(test_list)
 if __name__ == "__main__":
   main()
